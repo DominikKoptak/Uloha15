4f03e71962b7b3680f44fb041dcc7e9370187b76
DominikKoptak <127782135+DominikKoptak@users.noreply.github.com>
2023-05-10 01:41:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function myFunction(p1=4,p2=2){
+    
+    console.log( p1 + " " + p2 +" Vykonávam matematické operácie" 
+     + "\n" +p1 + "+" + p2 +"="+ (p1+p2)+ "\n" +p1 + "-" + p2 +"="+ (p1+p2)+ "\n" +p1 + "*" + p2 +"="+ (p1*p2)+ "\n" +p1 + "/" + p2 +"="+ (p1/p2));
+    }
+
+
+myFunction();
+
+myFunction(8,10);
